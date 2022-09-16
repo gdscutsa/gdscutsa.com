be1577ab0736e9040b08047a8b188512779f9801
Zack Umar <zackumar2@gmail.com>
2022-09-16 10:17:47 -0500

    Change Kickoff Location

--- app/constants/events.tsx
+++ app/constants/events.tsx
@@ -9,7 +9,7 @@ export type EventType = {
 export const UpcomingEvents: EventType[] = [
   {
     title: 'Kickoff!',
-    subtitle: 'Sept 28, 2022 - 1PM | SU 2.01',
+    subtitle: 'Sept 28, 2022 - 1PM | SU 2.02.12',
     description:
       'Come join us for our first event of the chapter! Find out what we are and who we are at this joint GDSC x ACM event!',
     image: 'assets/images/projects_background.webp',
