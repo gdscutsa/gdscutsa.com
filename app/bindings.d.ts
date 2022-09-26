9189e52230e0717f1c317be563ba5d7def1822b3
Zack Umar <zackumar2@gmail.com>
2022-09-26 16:44:14 -0500

    Add bindings

---
+++ app/bindings.d.ts
@@ -0,0 +1,7 @@
+export {};
+
+declare global {
+  const CF_PAGES: string;
+  const CONTENTFUL_SPACE_ID: string;
+  const CONTENTFUL_ACCESS_TOKEN: string;
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,8 +5,6 @@ import Header from '~/components/Header';
 import { DISCORD_LINK, GDSC_INFO_LINK } from '~/constants/links';
 import { SEO_DESCRIPTION } from '~/constants/seo';
 
-declare var CF_PAGES: string;
-
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'GDSC | UTSA - Home Page',
