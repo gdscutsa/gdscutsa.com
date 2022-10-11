f1cb1d522132375b7bd85bee0f5d9c7882aa6d5d
Zack Umar <zackumar2@gmail.com>
2022-10-11 09:10:29 -0500

    Add dynamic virtual flyer

--- app/models/contentful.server.tsx
+++ app/models/contentful.server.tsx
@@ -19,7 +19,7 @@ async function apiCall(
 export async function getUpcomingEvents(space: string, token: string) {
   const query = `
   {
-    eventCollection(order: date_DESC, where: {isPast: false}) {
+    eventCollection(order: date_DESC, where: {endDate_gte: "${new Date().toISOString()}"}) {
       items {
         name
         location
@@ -65,7 +65,7 @@ export async function getUpcomingEvents(space: string, token: string) {
 export async function getPastEvents(space: string, token: string) {
   const query = `
   {
-    eventCollection(order: date_DESC, where: {isPast: true}) {
+    eventCollection(order: date_DESC, where: {endDate_lt: "${new Date().toISOString()}"}) {
       items {
         name
         location
@@ -108,4 +108,60 @@ export async function getPastEvents(space: string, token: string) {
   return Promise.all(formattedData);
 }
 
-export const client = { getUpcomingEvents, getPastEvents };
+export type EventType = {
+  name: string;
+  slug: string;
+  location: string;
+  date: Date;
+  endDate: Date;
+  desc: string;
+  eventLink: string;
+  isPast: boolean;
+};
+
+export async function getEventBySlug(
+  space: string,
+  token: string,
+  slug: string
+): Promise<EventType[]> {
+  const query = `
+  {
+    
+    eventCollection(where: {slug: "${slug}", endDate_gte: "${new Date().toISOString()}"}) {
+      items {
+          name
+          location
+          date
+          endDate
+          desc 
+          eventLink
+        }
+    }
+  }`;
+
+  const response = await apiCall(space, token, query);
+  const json: any = await response.json();
+  const formattedData = await json.data.eventCollection.items.map(
+    async (project: {
+      name: string;
+      location: string;
+      date: any;
+      endDate: any;
+      desc: any;
+      eventLink: string;
+    }) => {
+      const { name, location, date, endDate, desc, eventLink } = project;
+      return {
+        name,
+        location,
+        date: new Date(date),
+        endDate: new Date(endDate),
+        desc,
+        eventLink,
+      };
+    }
+  );
+  return Promise.all(formattedData);
+}
+
+export const client = { getUpcomingEvents, getPastEvents, getEventBySlug };
