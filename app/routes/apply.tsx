84047d76b26e026191271e3ad7e24a55c01b4a2d
Zack Umar <zackumar2@gmail.com>
2022-10-26 13:50:32 -0500

    Fix seo for apply

--- app/routes/apply.tsx
+++ app/routes/apply.tsx
@@ -6,7 +6,7 @@ import { SEO_DESCRIPTION } from '~/constants/seo';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'GDSC | UTSA - Projects Page',
+  title: 'GDSC | UTSA - Apply',
   viewport: 'width=device-width,initial-scale=1',
   description: SEO_DESCRIPTION,
 });
